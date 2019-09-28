6aed872509d9022b95431b226ed1191c791cbfdd
COKI <RicardoBernal72>
2019-09-28 15:16:10 -0500

    se corrigio el problema2_3

--- libro/problemas_resueltos/Capitulo2/problema2_3.py
+++ libro/problemas_resueltos/Capitulo2/problema2_3.py
@@ -3,8 +3,8 @@ B=float(input("Variable b: "))
 C=float(input("variable c: "))
 HOLA=B**2 -4*A*C
 if HOLA>=0:
-    X1=((-B)+HOLA*.5)/2*A
-    X2=((-B)-HOLA*.5)/2*A
+    X1=((-B)+HOLA**(1/2))/(2*A)
+    X2=((-B)-HOLA**(1/2))/(2*A)
     print(f"Las incognitas de tu ecuacion son X1= {X1} y X2= {X2}.")
 if HOLA<0:
     print(f"no se puede escribir las raices puesto que {HOLA} es negativo.")
