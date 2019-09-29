f92482d68254abf71dd7c4406b1ac0f79055b106
COKI <RicardoBernal72>
2019-09-28 20:31:06 -0500

    se ralizo el problema 2.4 del libro

--- libro/problemas_resueltos/Capitulo2/problema2_4.py
+++ libro/problemas_resueltos/Capitulo2/problema2_4.py
@@ -1,10 +1,12 @@
-VALOR=str(input("asigna el valor: "))
-if VALOR =A :
-    print("accion 1")
-if VALOR =B :
-    print("accion 2")
-if VALOR =C :
-    print("accion 2")
+CAL1=float(input("primer calificacion: "))
+CAL2=float(input("segunda calificacion: "))
+CAL3=float(input("tercer calificacion: "))
+CAL4=float(input("cuarta calificacion: "))
+CAL5=float(input("quinta calificacion: "))
+PRO=(CAL1 + CAL2 + CAL3 + CAL4 + CAL5)/5
+if PRO>=6:
+    print(f"Bien aventurado sea usted ha pasado la materia con un promedio de {PRO} ")
 else:
-    print("accion x")
+    print(f"UFFF, no ha pasado la materia puesto que su promedio ha sido de {PRO}  ")
+
 
