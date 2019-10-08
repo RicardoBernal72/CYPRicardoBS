853a7a12ddc817714c3302600b0df9bdc52243e0
COKI <RicardoBernal72>
2019-10-08 17:44:00 -0500

    un ejemplo incompleto y informacion de if

---
+++ libro/ejemplo2_10.py
@@ -0,0 +1,17 @@
+A=int(input"Dame un numero: "))
+B=int(input"Dame un numero: "))
+C=int(input"Dame un numero: "))
+if A>B :
+    if A>C:
+        if B>c:
+            print(f"{A} > {B} > {C}")
+        else:
+            print(f"{A} > {C} > {B}")
+    else:
+        print(f"{C} > {A} > {B}")
+else:
+    if B>C:
+        if A>C:
+            print(f"{B} > {A} > {C}")
+        else:
+            print(f"{B} > {C} > {A}")
---
+++ switch.py
@@ -0,0 +1,14 @@
+Dia = int(input("introduce un dia del 1 al 5: "))
+if Dia==1:
+    print("lunes")
+elif Dia==2:
+    print("Martes")
+elif Dia==3:
+    print("Miercoles")
+elif Dia==4:
+    print("Jueves")
+elif Dia==5:
+    print("Viernes")
+else:
+    print("opcion no válida")
+print("Fin del programa!!")
