4153bf0865072e4a68c3ec4d24c553b574cd34ff
COKI <RicardoBernal72>
2019-10-22 17:44:29 -0500

    mas for y lista

---
+++ libro/problemas_resueltos/Capitulo3/problema3_2.py
@@ -0,0 +1,12 @@
+BAND=True
+SUMERSER=0
+for i in range(1801):
+    SUMERSER=SUMERSER+1
+    print(i)
+    if BAND==True:
+        BAND==False
+        i=i+3
+    else:
+        BAND==True
+        i=i+2
+print(SUMERSER)
